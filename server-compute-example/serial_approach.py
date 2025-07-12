f76dd13a34a1a9bcc5a6467f922adb9c76ef6673
anordin95 <alexander.f.nordin@gmail.com>
2025-07-12 13:14:43 -0700

    Small cleanup.

--- server-compute-example/serial_approach.py
+++ server-compute-example/serial_approach.py
@@ -16,14 +16,15 @@ start_time = time.time()
 global_start_time = start_time
 client = socket.socket()
 client.connect(server.SERVER_ADDRESS)
-print(f"Requesting computation from server.")
+print(f"Beginning server_request.")
 response = client.recv(4096)
-print(f"Server returned {float(response.decode()):.2f}. Ran for: {time.time() - start_time:.2f}s.")
+print(f"====== Done server_request. total: {float(response.decode()):.2f}. Ran for: {time.time() - start_time:.2f}s. ======")
 
 # Perform another computation directly.
 start_time = time.time()
+print(f"Beginning uniform_sum.")
 total = uniform_sum(n_samples=int(1.2e8))
-print(f"Computed total: {total:.2f} in {time.time() - start_time:.2f}s.")
+print(f"====== Done uniform_sum. total: {total:.2f}. Ran for: {time.time() - start_time:.2f}s. ======")
 
 
 print(f"Total time elapsed: {time.time() - global_start_time:.2f} seconds.")
